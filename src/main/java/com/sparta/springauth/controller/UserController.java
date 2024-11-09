a315e0d91b7a1465fc422cafbdcd9d3ba3a4df96
aeongiii <ljhkys6874@naver.com>
2024-11-09 15:34:48 +0900

    validation 예외 처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,44 +1,56 @@
 package com.sparta.springauth.controller;
 
-import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.parameters.P;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
-    // 주입받음
+    // 주입
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    // 로그인 페이지 반환
+    // 로그인 페이지
     @GetMapping("/user/login-page")
     public String loginPage() {
         return "login";
     }
 
-    // 회원가입 페이지 반환
+    // 회원가입 페이지
     @GetMapping("/user/signup")
     public String signupPage() {
         return "signup";
     }
 
-    // 회원가입 페이지
+    // 회원가입 페이지 반환
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) {
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
+
         return "redirect:/api/user/login-page";
     }
-
-
 }
